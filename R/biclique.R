6f021f8e0671d3ea8b34ebb6abbf73413bd4ceee
David Puelz <david.puelz@chicagobooth.edu>
2021-02-11 16:29:25 -0600

    Update biclique.R

--- R/biclique.R
+++ R/biclique.R
@@ -71,6 +71,8 @@ bi.clique <- function(filename, left_least = 1, right_least = 1, version = 1, fi
     # unlist biclique lists
     bi = list()
     for(i in 1:profile.raw[nelems-4]){
+	    cat(nelems,'\n')
+	    cat(i,'\n')
         temp1 = unlist(data.raw[[1]][[i]])
         temp2 = unlist(data.raw[[2]][[i]])
         idx = paste("biclique", i, sep="")
